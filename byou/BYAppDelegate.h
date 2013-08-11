5b3bfab32baa6f47de5368fd0a39197158285b77
Macroon70 <peter7012@icloud.com>
2013-08-11 15:10:08 +0200

    Initial Commit

---
+++ byou/BYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BYAppDelegate.h
+//  byou
+//
+//  Created by Peter on 2013.08.11..
+//  Copyright (c) 2013 LianDesign. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ byou/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  byou
+//
+//  Created by Peter on 2013.08.11..
+//  Copyright (c) 2013 LianDesign. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BYAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BYAppDelegate class]));
+    }
+}
